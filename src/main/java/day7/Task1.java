53febe726ab1277ee3906b3601261e981e17a474
Sergey Konovalov <redomi@ya.ru>
2022-04-25 16:12:07 +0300

    complete day7 tasks

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -3,5 +3,10 @@ package day7;
 public class Task1 {
     public static void main(String[] args) {
 
+        Airplane airplane1= new Airplane("Boeng",2011,127,7600);
+        Airplane airplane2= new Airplane("Ту-154",1998,131,8900);
+
+        Airplane.compareAirplanes(airplane1,airplane2);
+
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,33 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+
+        Random random = new Random();
+        int rand;
+        Player[] player = new Player[8];
+
+        for (int i = 0; i < 6; i++){ //добавляем игроков
+           rand = random.nextInt((Player.MAX_STAMINA - Player.MIN_STAMINA) + 1) + Player.MIN_STAMINA;
+           player[i] = new Player(rand);
+        }
+
+        Player.info();
+
+        //проверяем на добавление лишних игроков
+        rand = random.nextInt((Player.MAX_STAMINA - Player.MIN_STAMINA) + 1) + Player.MIN_STAMINA;
+        player[6] = new Player(rand);
+        rand = random.nextInt((Player.MAX_STAMINA - Player.MIN_STAMINA) + 1) + Player.MIN_STAMINA;
+        player[7] = new Player(rand);
+        System.out.println("Игроков на поле: " +Player.countPlayers);
+
+        for (int i = 0; i < 100; i++){ // игрок побежал
+            player[2].run();
+        }
+
+        Player.info();
 
     }
 }
