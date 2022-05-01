c4bd6cd92eb69882f1cb06a4c8eadadff93ef114
Sergey Konovalov <redomi@ya.ru>
2022-05-01 14:32:20 +0300

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,45 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args){
+
+        File fileIn = new File("src/main/resources/shoes.csv");
+        File fileOut = new File("src/main/resources/missing_shoes.txt");
+
+        if(!fileOut.exists()) {
+            try {
+                fileOut.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Невозможно создать файл");
+            }
+        }
+
+        try {
+            Scanner scanner = new Scanner(fileIn);
+            PrintWriter printWriter = new PrintWriter(fileOut);
+
+            while (scanner.hasNextLine()){
+
+                String[] lineArray = scanner.nextLine().split(";");
+
+                if(lineArray.length==3 && lineArray[2].equals("0")){
+
+                    printWriter.println(lineArray[0] + ";" + lineArray[1] + ";" + lineArray[2]);
+
+                }else {
+                    System.out.println("Неверный формат");
+                }
+            }
+
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
