5514fbde9c60ad723cd229febc34169a71405f16
Sergey Konovalov <redomi@ya.ru>
2022-05-05 15:25:26 +0300

    complete day19 tasks

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,33 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        File file = new File("src/main/resources/dushi.txt");
+        Scanner scanner = new Scanner(file);
+        scanner.useDelimiter("[….,«»„<>:;()?!\"\\s–]+");
+
+        Map<String, Integer> mapWords = new HashMap<>();
+
+        while (scanner.hasNext()){
+            String word = scanner.next().toLowerCase();
+
+            if(mapWords.get(word)==null)
+                mapWords.put(word,1);
+            else
+                mapWords.put(word,mapWords.get(word)+1);
+        }
+
+        mapWords.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .limit(100)
+                .forEach(System.out::println); //чичиков=608
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,11 +1,41 @@
 package day19.task2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        File file = new File("src/main/resources/taxi_cars.txt");
+        Scanner fileScan = new Scanner(file);
+
+        Map<Integer,Point> carMap = new HashMap<>();
+
+        while (fileScan.hasNextLine()){
+            String[] var = fileScan.nextLine().split(" ");
+            carMap.put(Integer.parseInt(var[0]),new Point(Integer.parseInt(var[1]),Integer.parseInt(var[2])));
+        }
+
+        Scanner input = new Scanner(System.in);
+        int x1 = input.nextInt();
+        int y1 = input.nextInt();
+        int x2 = input.nextInt();
+        int y2 = input.nextInt();
+        int x,y;
+
+        for (Map.Entry<Integer,Point> car:carMap.entrySet()){
+            x = car.getValue().getX();
+            y = car.getValue().getY();
+
+            if((x<x1 && y>y1) && (x>x2 && y<y2)){
+                System.out.println(car.getKey() + " " + x + " " + y);
+            }
+        }
     }
 }
-
